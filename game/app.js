afdce24314b1cdd8f9cfc6d4ebd8fb01075a6dc8
dyzalonius <dyzalonius@gmail.com>
2018-07-16 17:51:41 +0200

    Changed board size to match concept. Added side colors. Removed redundant 'players' (from tutorial). Added ship moving with the mouse (snapping to grid). Added ship rotation with R. Added game reset with O.

--- game/app.js
+++ game/app.js
@@ -29,30 +29,6 @@ io.sockets.on('connection', function (client) {
     // initialize a player for the client
     Player.onConnect(client);
 
-    // listen for input mousedown
-    client.on('mousedown', function (data) {
-        /*if (over a ship) {
-            copy it and move it with the mouse
-        }*/
-
-        // listen for input keydown
-        client.on('keydown', function () {
-            /* if (you press R) {
-                Ship.rotate(id of ship);
-            }*/
-        });
-
-        // listen for input mouseup
-        client.on('mouseup', function () {
-            /*remove the copy
-
-            if (position is valid spot for ship) {
-                move the original
-            }
-            */
-        });
-    });
-
     // listen for disconnect
     client.on('disconnect', function () {
         console.log('Client disconnected. (' + client.id + ')');
@@ -70,97 +46,104 @@ var Entity = function () {
     var self = {
         posX: 0,
         posY: 0,
+        width: 0,
+        height: 0,
         speedX: 0,
         speedY: 0,
         id: ""
-    }
-
-    self.update = function () {
-        self.updatePosition();
-    }
-
-    self.updatePosition = function () {
-        self.posX += self.speedX;
-        self.posY += self.speedY;
     }
 
     return self;
 }
 
 // SHIP
-var Ship = function (posX, posY, direction) {
+var Ship = function (posX, posY, direction, side) {
     var self = Entity();
 
     self.posX = posX;
     self.posY = posY;
-    self.width = 0;
-    self.height = 0;
     self.headingOffsetX = 0;
     self.headingOffsetY = 0;
-    self.isVertical = true;
-    self.isFacingTrue = true;
     self.direction = direction;
+    self.side = side;
+    self.childOffset = [0, 0];
 
     self.id = nextShipID;
     nextShipID++;
     SHIP_LIST[self.id] = self;
 
-    self.rotate = function () {
+    self.checkOverlap = function (pos) {
+        if (pos[0] > (self.posX)
+            && pos[0] < (self.posX + self.width)
+            && pos[1] > (self.posY)
+            && pos[1] < (self.posY + self.height)) {
+            return true;
+        }
+        return false;
+    }
+
+    self.rotate = function (pos) {
         // change direction
-        direction++;
-        if (direction == 4) {
-            direction = 0;
+        self.direction++;
+        if (self.direction == 4) {
+            self.direction = 0;
         }
 
         self.updateDirection();
-    };
+        self.setPosOffset(pos);
+    }
 
     self.updateDirection = function () {
-        // update direction
-        switch (direction) {
+        // update childOffset
+        var temp = self.childOffset[0];
+        self.childOffset[0] = self.height - self.childOffset[1];
+        self.childOffset[1] = temp;
+
+        // update dimensions
+        switch (self.direction) {
             case 0:
-                isVertical = true;
-                isFacingTrue = false;
+                self.width = 50;
+                self.height = 150;
+                self.headingOffsetX = 0;
+                self.headingOffsetY = 0;
                 break;
 
             case 1:
-                isVertical = false;
-                isFacingTrue = true;
+                self.width = 150;
+                self.height = 50;
+                self.headingOffsetX = self.width - 50;
+                self.headingOffsetY = self.height - 50;
                 break;
 
             case 2:
-                isVertical = true;
-                isFacingTrue = true;
+                self.width = 50;
+                self.height = 150;
+                self.headingOffsetX = self.width - 50;
+                self.headingOffsetY = self.height - 50;
                 break;
 
             case 3:
-                isVertical = false;
-                isFacingTrue = false;
+                self.width = 150;
+                self.height = 50;
+                self.headingOffsetX = 0;
+                self.headingOffsetY = 0;
                 break;
         }
+    }
 
-        // change dimensions
-        if (isVertical) {
-            self.width = 50;
-            self.height = 150;
-        } else {
-            self.width = 150;
-            self.height = 50;
-        }
+    self.setPosOffset = function (pos) {
+        self.posX = (pos[0] - self.childOffset[0]);
+        self.posY = (pos[1] - self.childOffset[1]);
+    }
 
-        // change heading
-        if (isFacingTrue) {
-            self.headingOffsetX = self.width - 50;
-            self.headingOffsetY = self.height - 50;
-        } else {
-            self.headingOffsetX = 0;
-            self.headingOffsetY = 0;
-        }
+    self.setPosGrid = function () {
+        self.posX = Math.round(self.posX / 50) * 50;
+        self.posY = Math.round(self.posY / 50) * 50;
     }
 
     self.updateDirection();
 }
-Ship.spawn = function (gridX, gridY, direction) {
+Ship.spawn = function (gridX, gridY, direction, side) {
     var posX = gridX * 50;
     var posY = gridY * 50;
 
@@ -185,11 +168,11 @@ Ship.spawn = function (gridX, gridY, direction) {
     }
 
     // create new ship
-    var ship = Ship(posX, posY, direction, isVertical, isFacingTrue);
+    var ship = Ship(posX, posY, direction, side);
 }
-Ship.rotate = function (id) {
+Ship.rotate = function (id, pos) {
     var ship = SHIP_LIST[id];
-    ship.rotate();
+    ship.rotate(pos);
 }
 Ship.update = function () {
     var package = [];
@@ -198,9 +181,6 @@ Ship.update = function () {
     for (var i in SHIP_LIST) {
         var ship = SHIP_LIST[i];
 
-        // change ship's position
-        ship.update();
-
         // add ship's position to the package
         package.push({
             posX: ship.posX,
@@ -208,50 +188,32 @@ Ship.update = function () {
             width: ship.width,
             height: ship.height,
             headingOffsetX: ship.headingOffsetX,
-            headingOffsetY: ship.headingOffsetY
+            headingOffsetY: ship.headingOffsetY,
+            side: ship.side
         });
     }
 
     return package;
+}
+Ship.checkOverlap = function (pos) {
+    for (var i in SHIP_LIST) {
+        var ship = SHIP_LIST[i];
+        if (ship.checkOverlap(pos)) {
+            return ship;
+        }
+    }
+    return null;
 }
 
 // PLAYER
 var Player = function (id) {
     var self = Entity();
 
-    self.posX = 250;
-    self.posY = 250;
     self.id = id;
     self.number = "" + Math.floor(10 * Math.random());
-    self.pressingRight = false;
-    self.pressingLeft = false;
-    self.pressingUp = false;
-    self.pressingDown = false;
-    self.maxSpd = 5;
     PLAYER_LIST[id] = self;
-
-    self.update = function () {
-        self.updateSpeed();
-        self.updatePosition();
-    }
-
-    self.updateSpeed = function () {
-        if (self.pressingRight) {
-            self.speedX = self.maxSpd;
-        } else if (self.pressingLeft) {
-            self.speedX = -self.maxSpd;
-        } else {
-            self.speedX = 0;
-        }
-
-        if (self.pressingUp) {
-            self.speedY = -self.maxSpd;
-        } else if (self.pressingDown) {
-            self.speedY = self.maxSpd;
-        } else {
-            self.speedY = 0;
-        }
-    }
+    self.child = null;
+    self.lastMousePos = [0, 0];
 
     return self;
 }
@@ -259,62 +221,98 @@ Player.onConnect = function (client) {
     // create new player
     var player = Player(client.id);
 
-    // listen for input
+    // listen for input keypress
     client.on('keyPress', function (data) {
-        if (data.inputId === 'left') {
-            player.pressingLeft = data.state;
-        } else if (data.inputId === 'right') {
-            player.pressingRight = data.state;
-        } else if (data.inputId === 'up') {
-            player.pressingUp = data.state;
-        } else if (data.inputId === 'down') {
-            player.pressingDown = data.state;
+        if (data.inputId === 'R') {
+            if (player.child != null) {
+                player.child.rotate(player.lastMousePos);
+            }
+        }
+        if (data.inputId === 'O') {
+            GameStart();
         }
     });
+
+    client.on('mouseDown', function (data) {
+        player.lastMousePos = [data.posX, data.posY];
+        var ship = Ship.checkOverlap(player.lastMousePos);
+        if (ship != null) {
+            player.child = ship;
+            ship.childOffset = [(data.posX - ship.posX), (data.posY - ship.posY)];
+        }
+    });
+
+    client.on('mousePos', function (data) {
+        player.lastMousePos = [data.posX, data.posY];
+        if (player.child != null) {
+            player.child.setPosOffset(player.lastMousePos);
+        }
+    });
+
+    client.on('mouseUp', function (data) {
+        if (player.child != null) {
+            player.child.setPosGrid();
+            player.child = null;
+        };
+    });
+
+    // listen for input mousedown
+    /*client.on('mousedown', function (data) {
+        if (over a ship) {
+            copy it and move it with the mouse
+        }
+
+        // listen for input keydown
+        client.on('keyPress', function () {
+            if (you press R) {
+                Ship.rotate(id of ship);
+            }
+        });
+
+        // listen for input mouseup
+        client.on('mouseup', function () {
+            remove the copy
+
+            if (position is valid spot for ship) {
+                move the original
+            }
+            
+        });
+    });*/
 }
 Player.onDisconnect = function (client) {
     delete PLAYER_LIST[client.id];
 }
-Player.update = function () {
-    var package = [];
 
-    // add positions of all players to the package
-    for (var i in PLAYER_LIST) {
-        var player = PLAYER_LIST[i];
+////////////////////////////////////////
+//           SUB FUNCTIONS            //
+////////////////////////////////////////
 
-        // change player's position
-        player.update();
+var GameStart = function () {
+    SHIP_LIST = {};
+    nextShipID = 0;
 
-        // add player's position to the package
-        package.push({
-            posX: player.posX,
-            posY: player.posY,
-            number: player.number
-        });
-    }
-
-    return package;
+    // spawn all ships
+    [[1, 5, 0, 0], [5, 8, 3, 0], [2, 1, 1, 1], [8, 2, 2, 1]].forEach(element => {
+        Ship.spawn(element[0], element[1], element[2], element[3]);
+    });
+    console.log('Game start.');
 }
 
 ////////////////////////////////////////
 //             GAME LOOP              //
 ////////////////////////////////////////
 
-var nextShipID = 0;
 var CLIENT_LIST = {};
 var PLAYER_LIST = {};
-var SHIP_LIST = {};
+var SHIP_LIST;
+var nextShipID;
 
-// spawn all ships
-[[2, 11, 0]].forEach(element => {
-    Ship.spawn(element[0], element[1], element[2]);
-});
+GameStart();
 
 // start loop which ticks every 40ms
 setInterval(function () {
-    var package = {};
-    package[0] = Player.update();
-    package[1] = Ship.update();
+    var package = Ship.update();
 
     // send all clients the package
     for (var i in CLIENT_LIST) {
